95f88b9dbc4110420c5e19cc7027c8c7bf217957
kym <ofxiaoka@163.com>
2018-03-28 09:57:07 +0800

    返回顶部

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
     'use strict';
     var asiderbar = $('#asidebar'),  //选择侧栏
         mask = $('#mask'),
-        bar_trigger = $('#bar_trigger');
+        bar_trigger = $('#bar_trigger'),
+        back_button = $('.return-top');
         function inasiderbar() {
             mask.fadeIn();
             asiderbar.css('right', 0);
@@ -11,6 +12,18 @@
             mask.fadeOut();
             asiderbar.css('right', -asiderbar.width());
         }
+
     bar_trigger.on('click', inasiderbar);
     mask.on('click', outasiderar);
+    back_button.on('click', function () {
+        $('html,body').animate({
+            scrollTop:0
+        }, 800)
+    });
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > $(window).height()){
+            back_button.fadeIn();
+        }else
+            back_button.fadeOut();
+    })
 })
